7dd36269d7665658d72f1d5126a9f39db4512410
Omar-Codess <omartaki8@gmail.com>
2022-10-28 20:07:44 +0200

    generate 10 email in console

--- js/script.js
+++ js/script.js
@@ -1,8 +1,49 @@
-let newEmail;
-axios
-    .get("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then(function (resp){
-        console.log("fatto!");
-        newEmail = resp.data;
-        console.log(newEmail.response);
-    });
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            emailsList: [],
+        };
+    },
+    methods: {
+        getEmails() {
+            for(let i = 0; i < 10; i++){
+                axios
+                    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then((resp) =>{
+                        this.emailsList.push(resp.data.response);
+                    });
+            }
+        },
+    },
+    created(){
+        this.getEmails()
+    }
+}).mount("#app");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let newEmail;
+// axios
+//     .get("https://flynn.boolean.careers/exercises/api/random/mail")
+//     .then(function (resp){
+//         console.log("fatto!");
+//         newEmail = resp.data;
+//         console.log(newEmail.response);
+//     });
